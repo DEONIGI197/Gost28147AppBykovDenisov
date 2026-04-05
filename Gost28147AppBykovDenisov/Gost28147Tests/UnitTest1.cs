93f7f8ef9b699bd21d5ffdb0a4270ee9d74025db
DEONIGI BYKOV <deonigi@mail.ru>
2026-04-05 21:33:19 +0300

    Добавлен файл REAME + выполнены тесты

--- Gost28147AppBykovDenisov/Gost28147Tests/UnitTest1.cs
+++ Gost28147AppBykovDenisov/Gost28147Tests/UnitTest1.cs
@@ -1,11 +1,15 @@
+using System;
+using Xunit;
+
 namespace Gost28147Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void SimpleTest_ShouldPass()
         {
-
+            // Простейший тест, который всегда проходит
+            Assert.True(true);
         }
     }
 }
